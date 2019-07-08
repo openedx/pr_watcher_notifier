5464c7e547b018bfb33fa97ed9c503e949d187bc
Guruprasad Lakshmi Narayanan <guruprasad@opencraft.com>
2019-07-09 00:50:30 +0530

    Conditionally define MAIL_* settings if present in environment
    Only MAIL_DEFAULT_SENDER is required

--- settings.py
+++ settings.py
@@ -32,9 +32,19 @@ GITHUB_ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
 
 WATCH_CONFIG = get_watch_config()
 
-MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-MAIL_SERVER = os.environ.get('MAIL_SERVER')
-MAIL_PORT = os.environ.get('MAIL_PORT')
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = os.environ('MAIL_DEFAULT_SENDER')
+
+if os.environ.get('MAIL_SERVER'):
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+
+if os.environ.get('MAIL_PORT'):
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+
+if os.environ.get('MAIL_USE_TLS'):
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+
+if os.environ.get('MAIL_USERNAME'):
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+
+if os.environ.get('MAIL_PASSWORD'):
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
